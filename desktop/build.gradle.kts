48968038e2132963baac963a056406d68db6588c
Yonatan Karp-Rudin <yonvata@gmail.com>
2026-06-04 16:55:30 +0200

    ci: include desktop in coverage reporting
    Closes #121

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.compose.multiplatform)
     alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.kover)
 }
 
 kotlin {
